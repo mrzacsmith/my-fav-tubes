ab94e030538bf82f270328754d22c44b7b5492f5
Zac Smith <zrs3141592@gmail.com>
2020-09-26 13:14:10 -0600

    server update and running

--- server/server.js
+++ server/server.js
@@ -1,10 +1,36 @@
 const express = require('express')
-const colors = require('colors')
+const cors = require('cors')
+const helmet = require('helmet')
+const morgan = require('morgan')
+
+const connectDB = require('./utils/db.js')
+
+require('dotenv').config()
+require('colors')
 
 const server = express()
+server.use(express.json())
+server.use(helmet())
+server.use(morgan('dev'))
+server.use(cors())
+
+connectDB()
+
+const currentTime = new Date().toLocaleString('en-US', {
+  timeZone: 'America/Denver',
+})
+
+server.get('/', (req, res) => {
+  res.status(200).json({
+    status: 'Success',
+    message: 'Server is live!',
+    data: currentTime + ' MST',
+    author: 'Github: @mrzacsmith',
+  })
+})
 
 const PORT = process.env.PORT || 3333
 
 server.listen(PORT, () => {
-  console.log(`** Server is listening on port ${PORT}`.rainbow)
+  console.log(`\n** Server is listening on port ${PORT}`.rainbow)
 })
