9dab33ecbe65e3dc99212726638e1ae58367a1cd
Zac Smith <zrs3141592@gmail.com>
2020-09-26 13:13:49 -0600

    connection to db

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,1 +1,18 @@
-// connectDB
+const mongoose = require('mongoose')
+const colors = require('colors')
+
+const connectDB = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    })
+    console.log(`** MongoDB Connected: ${connect.connection.host}\n`.rainbow)
+  } catch (error) {
+    console.log(colors(error))
+    process.exit(1)
+  }
+}
+
+module.exports = connectDB
